c640b031f13c96a5bee8e36f444137e07633caac
A.K.SAJIN <108419506+sajin-oops@users.noreply.github.com>
2024-08-09 22:02:31 +0530

    Add files via upload

---
+++ function.py
@@ -0,0 +1,27 @@
+#function - A block of reusable code
+# Place () after the function name to invoke it
+
+def happy_birthday(name):
+  print(f"Happy birthday to {name}!")
+  print("You are old!")
+  print("Happy birthday to you!")
+  print()
+  
+happy_birthday("Sajin")
+happy_birthday("Lijin")
+happy_birthday("Aaron")
+
+#O/P
+'''
+Happy birthday to Sajin!
+You are old!
+Happy birthday to you!
+
+Happy birthday to Lijin!
+You are old!
+Happy birthday to you!
+
+Happy birthday to Aaron!
+You are old!
+Happy birthday to you!
+'''
